14ba81c45b4df72152bed0d589dab284a4d6b913
Andy Vo <andy.v.vo2004@gmail.com>
2024-03-26 20:05:03 -0400

    added email/password fields to login page

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+
+
+export default function Login() {
+    return (
+        <>
+            <div class="border-b-8">top bar</div>
+            <div class="flex flex-col items-center">
+                <div class="text-5xl font-bold mt-48">Login</div>
+                <input type="text" placeholder="Email" class="border-b-2 border-red-500 w-1/3 mt-8 text-xl"/>
+                <input type="text" placeholder="Password" class="border-b-2 border-red-500 w-1/3 mt-6 text-xl"/>
+            </div>
+        </>
+    )
+}6
