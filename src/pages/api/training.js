a4c1969c2c06b3a4efd54497e57a580bddf57467
Tanmay102 <tanmaygupta102@gmail.com>
2024-03-28 17:36:52 -0400

    Finish update api calls

--- src/pages/api/training.js
+++ src/pages/api/training.js
@@ -1,4 +1,5 @@
 import createTrainingLog from "../../../server/mongodb/actions/createTrainingLog";
+import updateTrainingLog from "../../../server/mongodb/actions/updateTrainingLog";
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
@@ -8,6 +9,18 @@ export default async function handler(req, res) {
                 return res.status(400).send("Please check body and enter all required fields appropriately");
             } else {
                 await createTrainingLog({user, animal, title, date, description, hours});
+                return res.status(200).send("Success");
+            }
+        } catch (error) {
+            return res.status(500).send("Failed");
+        }
+    } else if (req.method === "PATCH") {
+        try {
+            const {logID, hours} = req.body;
+            if (!logID || !hours) {
+                return res.status(400).send("Please ensure you have entered the training log and new hours appropriately");
+            } else {
+                await updateTrainingLog({trainingLog : logID, newHours : hours});
                 return res.status(200).send("Success");
             }
         } catch (error) {
