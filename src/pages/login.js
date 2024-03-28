72fea5a64831475bef04b01a01b824955f1b1898
Andy Vo <andy.v.vo2004@gmail.com>
2024-03-28 18:36:32 -0400

    finished login page ui

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,11 +3,17 @@
 export default function Login() {
     return (
         <>
-            <div class="border-b-8">top bar</div>
-            <div class="flex flex-col items-center">
-                <div class="text-5xl font-bold mt-48">Login</div>
-                <input type="text" placeholder="Email" class="border-b-2 border-red-500 w-1/3 mt-8 text-xl"/>
-                <input type="text" placeholder="Password" class="border-b-2 border-red-500 w-1/3 mt-6 text-xl"/>
+            <div className="border-b-8">top bar</div>
+            <div className="flex flex-col items-center">
+                <div className="text-5xl font-bold mt-40">Login</div>
+                <input type="text" placeholder="Email" class="border-b-2 border-red-600 w-1/3 mt-8 text-xl"/>
+                <input type="text" placeholder="Password" class="border-b-2 border-red-600 w-1/3 mt-6 text-xl"/>
+                <button className="bg-red-600 text-white mt-12 text-2xl pt-1 pb-1.5 px-56 rounded-xl">Log In</button>
+                <div className="mt-5">Don't have an account? <a href="/signup" className="font-bold">Sign up</a></div>
+            </div>
+            <div className="fixed bottom-0 flex flex-col items-center w-full py-4 text-sm text-gray-600">
+                <div>Made by Team Cheetah</div>
+                <div>2023 BOG Developer Bootcamp</div>
             </div>
         </>
     )
