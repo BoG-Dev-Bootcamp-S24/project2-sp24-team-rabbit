234915b15f33cb28073cdb129fc55ab6e4ff4189
Andy Vo <andy.v.vo2004@gmail.com>
2024-03-28 19:22:01 -0400

    completed signup page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
-
+import Image from "next/image"
+import quarterCircle from "../../public/images/quarterCircle.png"
 
 export default function Login() {
     return (
@@ -11,6 +12,7 @@ export default function Login() {
                 <button className="bg-red-600 text-white mt-12 text-2xl pt-1 pb-1.5 px-56 rounded-xl">Log In</button>
                 <div className="mt-5">Don't have an account? <a href="/signup" className="font-bold">Sign up</a></div>
             </div>
+            <Image src={quarterCircle} alt="quarter circle" className="fixed bottom-0" width={180} height={180}/>
             <div className="fixed bottom-0 flex flex-col items-center w-full py-4 text-sm text-gray-600">
                 <div>Made by Team Cheetah</div>
                 <div>2023 BOG Developer Bootcamp</div>
