a6b459a53a2212e2f6507afd45281214b325cf24
Nathan <nathandong68@gmail.com>
2024-04-02 18:21:11 -0400

    top banner complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import appLogo from '/public/images/appLogo.png'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="bg-white w-screen h-screen">
-      <div className="h-[10%] border-b-2 bg pl-[10%] flex content-start">
-        
+      <div className="h-[10%] border-b-2 bg pl-[2%] flex flex-row items-center p-[1.2%]">
+        <Image src={appLogo} alt="App logo" className="object-contain pr-[1%]"/>
+        <h1 className="text-black text-4xl">Progress</h1>
       </div>
     </div>
   );
