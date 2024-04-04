f590dc1f01d5f10fe745dec135ea5ab795c0a20c
Nathan <nathandong68@gmail.com>
2024-04-04 02:02:57 -0400

    signup page ui complete

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import TopBanner from "../components/TopBanner.js"
+import CreateAccount from "../components/CreateAccount.js";
 import quarterCircle from '/public/images/quarterCircle.png';
 import Navbar from "@/components/Navbar/Navbar.js";
 
@@ -8,7 +10,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function SignUp() {
   return (
     <div className="bg-white w-screen h-screen flex flex-col justify-between items-center">
-      <Navbar searchBar={false}/>
+      <TopBanner searchBar={false}/>
+      <CreateAccount/>
       
       <div className="flex justify-start w-[100%]">
         <Image src={quarterCircle} alt="background image" className="object-contain"/>
