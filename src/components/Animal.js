0f0308819ee8c8dd00fcb24c4d69fa618920e863
Nathan <nathandong68@gmail.com>
2024-04-08 13:43:22 -0400

    animalList header create new animal button

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -6,7 +6,7 @@ export default function Animal(props) {
     const {animal} = props;
     
     return(
-        <div className="flex flex-col justify-between items-center w-[28%] aspect-square text-black rounded-xl shadow-lg mx-[1.5%] mt-[3%]">
+        <div className="flex flex-col justify-between items-center w-[28%] aspect-square text-black rounded-xl shadow-lg mx-[1.5%] mb-[3%]">
             <Image src={animal.imageURL} width={0} height={0} alt="Animal photo" sizes="100vw" className="object-cover w-[100%] h-[75%] rounded-t-xl"/>
             <div className="h-[25%] w-[100%] flex flex-row items-center justify-around p-[5%]">
                 <div className="bg-red-600 h-[80%] flex items-center justify-center aspect-square rounded-full font-extrabold text-white text-xl">{animal.name.substring(0, 1)}</div>
--- src/components/AnimalsList.js
+++ src/components/AnimalsList.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Animal from "../components/Animal"
+import createNewLogo from "/public/images/createNewLogo.png"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function AnimalsList() {
@@ -26,8 +27,14 @@ export default function AnimalsList() {
 
     return(
         <div className="w-[100%]">
-            <button>New</button>
-
+            <div className="w-[100%] border-b-2 pl-[2%] py-[1%] px-[10%] flex justify-between">
+                <h1 className="text-lg font-bold text-slate-500">Animals</h1>
+                <button className="text-slate-500 w-[10.5%] flex justify-between items-center whitespace-nowrap ">
+                    <Image src={createNewLogo} className="w-[18%]"/>
+                    Create New
+                </button>
+            </div>
+            
             <div className="w-[100%] flex flex-row justify-start items-start flex-wrap text-black m-[3%]">
                 {animalList == null ? <div>Loading...</div> : animalList[0] == null ? <div>No Animals Found</div> : 
                     animalList.map((animal) => 
