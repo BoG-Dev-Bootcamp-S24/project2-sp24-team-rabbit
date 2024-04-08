45bb4c72d62a6a2a36a404dbb807adcbfda6c74f
jsachdeva7 <jsachdeva7@gatech.edu>
2024-04-08 16:41:09 -0400

    Created top banner for animals list and training log pages.

--- src/pages/animalslist.js
+++ src/pages/animalslist.js
@@ -3,12 +3,19 @@ import { Inter } from "next/font/google";
 import quarterCircle from '/public/images/quarterCircle.png';
 import Navbar from "@/components/Navbar/Navbar.js";
 import SideBar from "@/components/Sidebar";
+import TopBanner from "@/components/TopBanner";
+// import AnimalsList from "@/components/AnimalsList";
 
 export default function AnimalsList() {
     return (
-        <div className="bg-white w-screen h-screen flex-row">
-          <Navbar searchBar={true} />
-          <SideBar />
+        <div className="bg-white w-screen h-screen flex flex-col relative">
+          <Navbar searchBar={true} className="z-30 relative" />
+          <div className="flex flex-row h-full">
+            <SideBar className="z-20 relative"/>
+            <div className="flex flex-col h-full w-full">
+              <TopBanner animalsList={true}/>
+            </div>
+          </div>
         </div>
     )
 }
