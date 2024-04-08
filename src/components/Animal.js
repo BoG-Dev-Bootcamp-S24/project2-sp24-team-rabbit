eac73630f9581fdd889e2d46e14c925b55fbbb75
Nathan <nathandong68@gmail.com>
2024-04-08 13:21:50 -0400

    animalList component displays work

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -6,11 +6,11 @@ export default function Animal(props) {
     const {animal} = props;
     
     return(
-        <div className="flex flex-col justify-between items-center aspect-square w-[20%] text-black rounded-xl shadow-lg">
+        <div className="flex flex-col justify-between items-center w-[28%] aspect-square text-black rounded-xl shadow-lg mx-[1.5%] mt-[3%]">
             <Image src={animal.imageURL} width={0} height={0} alt="Animal photo" sizes="100vw" className="object-cover w-[100%] h-[75%] rounded-t-xl"/>
             <div className="h-[25%] w-[100%] flex flex-row items-center justify-around p-[5%]">
                 <div className="bg-red-600 h-[80%] flex items-center justify-center aspect-square rounded-full font-extrabold text-white text-xl">{animal.name.substring(0, 1)}</div>
-                <div>
+                <div className="bg-white">
                     <div className="font-bold whitespace-nowrap">{animal.name} - {animal.breed}</div>
                     <div className="text-slate-500 text-sm">{animal.owner} • Trained: {animal.hours} hours</div>
                 </div>
--- src/components/AnimalsList.js
+++ src/components/AnimalsList.js
@@ -4,7 +4,9 @@ import Animal from "../components/Animal"
 const inter = Inter({ subsets: ["latin"] });
 
 export default function AnimalsList() {
-    //pass in true as a prop if page requires searchBar
+    let animalList = null;
+    //set animalList = database list
+    animalList = []
 
     let animal = {
         imageURL:"https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
@@ -13,10 +15,26 @@ export default function AnimalsList() {
         owner:"Nathan",
         hours:"20"
     }
-    
+
+    animalList[0] = animal
+    animalList[1] = animal
+    animalList[2] = animal
+    animalList[3] = animal
+    animalList[4] = animal
+    animalList[5] = animal
+    animalList[6] = animal
+
     return(
-        <div className="h-[100%] flex flex-row justify-between items-center p-[1.2%] w-full">
-            <Animal animal={animal}/>
+        <div className="w-[100%]">
+            <button>New</button>
+
+            <div className="w-[100%] flex flex-row justify-start items-start flex-wrap text-black m-[3%]">
+                {animalList == null ? <div>Loading...</div> : animalList[0] == null ? <div>No Animals Found</div> : 
+                    animalList.map((animal) => 
+                    <Animal animal={animal}/>
+                )}
+            </div>
+
         </div>
     );
 }
