3028a3e3ee605b2dce2eb253d491cb8dc46b5164
jsachdeva7 <jsachdeva7@gatech.edu>
2024-04-09 10:40:51 -0400

    Made SidebarButton and LogOut components more reusable.

--- src/components/Navbar/Searchbar.js
+++ src/components/Navbar/Searchbar.js
@@ -4,7 +4,7 @@ import searchLogo from "/public/images/searchLogo.png"
 
 export default function Searchbar() {
     return (
-        <form className="w-[35%] border flex flex-row rounded-lg">
+        <form className="w-[35%] border flex flex-row rounded-3xl p-1">
             <Image src={searchLogo} className="object-contain p-[1.5%]"/>
             <input className="bg-transparent w-[100%] text-black outline-none" name="search" placeholder="Search"/>
         </form>
