f5013244ade2e207f120f066eec335a7449a0707
Tanmay102 <tanmaygupta102@gmail.com>
2024-04-11 13:17:37 -0400

    Link login backend to frontend

--- src/pages/animalslist.js
+++ src/pages/animalslist.js
@@ -4,17 +4,20 @@ import Navbar from "@/components/Navbar/Navbar.js";
 import SideBar from "@/components/Sidebar/Sidebar";
 import TopBanner from "@/components/TopBanner/TopBanner";
 import AnimalsListContainer from "@/components/Animal Pages/AnimalsList";
+import { useRouter } from "next/router";
 
 
 export default function AnimalsList() {
-    const currentUser = "Nathan";
+    // const router = useRouter();
+    // const user = router.query.user;
+    const user = "Nathan"
     return (
       <div className="bg-white w-[100%] h-[100%] flex flex-col relative">
         <Navbar searchBar={true}/>
         <div className="w-full h-full flex flex-row">
-          <SideBar adminAccess={true} currentPage={"animalslist"} user={currentUser}/>
+          <SideBar adminAccess={true} currentPage={"animalslist"} user={user}/>
           <div className="flex flex-col w-full h-full">
-            <AnimalsListContainer user={currentUser} allAnimals={false}/>
+            <AnimalsListContainer user={user} allAnimals={false}/>
           </div>
         </div>
       </div>
