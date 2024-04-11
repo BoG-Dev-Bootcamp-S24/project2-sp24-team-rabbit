67209966e7e243b9334f9a1f52ea30384127dd7c
Tanmay102 <tanmaygupta102@gmail.com>
2024-04-11 13:35:40 -0400

    Finish up signup frontend-backend connection

--- src/components/LoginAndCreateAcc/CreateAccount.js
+++ src/components/LoginAndCreateAcc/CreateAccount.js
@@ -11,6 +11,43 @@ export default function CreateAccount() {
     const [currPass, setCurrPass] = useState("");
     const [currVerifyPass, setCurrVerifyPass] = useState("");
     const [admin, setAdmin] = useState(false);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!currName || !currEmail || !currPass || !currVerifyPass) {
+            alert('Please fill out all fields.');
+            return;
+        } else if (currPass != currVerifyPass) {
+            alert('Passwords need to match');
+            return;
+        }
+        const formData = {
+            fullName: currName,
+            email: currEmail,
+            password: currPass,
+            admin: admin
+        };
+        try {
+            const response = await fetch('http://localhost:3000/api/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.text();
+            console.log(result);
+            alert('Form submitted successfully! Please login now');
+            
+        } catch (error) {
+            console.error('Failed to submit the form:', error);
+            alert('There was a problem with your submission. Please try again.');
+        }
+    };
+
     
     return(
         <div className= "w-[70%] flex flex-col items-center mt-[5%]">
@@ -29,8 +66,7 @@ export default function CreateAccount() {
                 <input onClick={() => setAdmin(!admin)} type="checkbox" className="border border-red-600 accent-red-600 mt-4 checked:bg-rd-600 h-5 w-5"/>
                 <div className="text-black ml-3 mt-3.5"> Admin Access </div>
             </div>
-            <button type="submit" className="text-white text-xl font-bold mt-[7%] w-[100] p-[1%] rounded-lg bg-red-600"
-                onClick={() => false}>Sign up</button>
+            <button type="submit" onClick={handleSubmit} className="text-white text-xl font-bold mt-[7%] w-[100] p-[1%] rounded-lg bg-red-600">Sign up</button>
         </form>
 
         <div className="flex flex-row whitespace-nowrap mt-[7%]">
--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -6,9 +6,9 @@ export default function TopBanner({ formStatusProp, setFormStatusProp, title }) 
     <div className="flex flex-row border-b-2 pt-6 pb-2 h-auto w-[100%] justify-between items-end">
       <p className="ml-6 font-medium text-2xl" style={{ color: "rgb(128, 116, 116)" }}>{title}</p>
       {(title !== "All users" && title !== "All training") && (!formStatusProp &&
-        <button className="ml-1 font-medium flex flex-row items-center justify-center hover:bg-gray-100 mr-5 p-0.5 rounded-md" style={{ color: "rgb(128, 116, 116)" }} onClick={() => setFormStatusProp(true)}>
+        <button className="w-40 ml-1 font-medium flex flex-row items-center justify-center hover:bg-gray-100 mr-5 p-0.5 rounded-md" style={{ color: "rgb(128, 116, 116)" }} onClick={() => setFormStatusProp(true)}>
           <Image src={createNewLogo} className="w-[14%] mt-[2.25px]" />
-          {title !== "All animals" && <span className="ml-1">Create new</span>}
+          {title !== "All animals" && <span className="ml-3">Create new</span>}
         </button>
       )}
     </div>
